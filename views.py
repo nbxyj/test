20a3c0627a60fbe380a486ddb09dd0fe8cab1fa5
xuyj <openxyj@tom.com>
2019-07-17 04:56:44 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
-
+form django.shortcuts import redirect
 def index(request):
 	return HttpResponse('index')
+		
+def login(requets):
+	return redirect('/index')
+
 
